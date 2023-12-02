eb9bdc4545268df509f359f641e43784cb29d683
zji25 <xeniayarunina@gmail.com>
2023-12-02 13:37:44 +0300

    |

--- ITMO_DZ_4/ThreadSafeArray.swift
+++ ITMO_DZ_4/ThreadSafeArray.swift
@@ -2,20 +2,63 @@ import Foundation
 
 class ThreadSafeArray<T>  {
     private var array: [T] = []
+    private var rwLock = pthread_rwlock_t()
+    
+    init() {
+        pthread_rwlock_init(&rwLock, nil)
+    }
+
+    deinit {
+        pthread_rwlock_destroy(&rwLock)
+    }
+    
+    private func readLock() {
+       pthread_rwlock_rdlock(&rwLock)
+    }
+
+    private func writeLock() {
+        pthread_rwlock_wrlock(&rwLock)
+    }
+
+    private func unlock() {
+        pthread_rwlock_unlock(&rwLock)
+    }
 }
 
 extension ThreadSafeArray: RandomAccessCollection {
     typealias Index = Int
     typealias Element = T
+    
+    var startIndex: Index {
+        readLock()
+        defer { unlock() }
+        return array.startIndex
+    }
 
-    var startIndex: Index { return array.startIndex }
-    var endIndex: Index { return array.endIndex }
-
+    var endIndex: Index {
+        readLock()
+        defer { unlock() }
+        return array.endIndex
+    }
+    
     subscript(index: Index) -> Element {
-        get { return array[index] }
+        get {
+            readLock()
+            defer { unlock() }
+            return array[index]
+        }
     }
 
     func index(after i: Index) -> Index {
+        readLock()
+        defer { unlock() }
         return array.index(after: i)
     }
+
+    func append(_ element: T) {
+        writeLock()
+        defer { unlock() }
+        array.append(element)
+    }
+
 }
--- ITMO_DZ_4/main.swift
+++ ITMO_DZ_4/main.swift
@@ -1,9 +1,25 @@
 import Foundation
 
-let ff = ThreadSafeArray<Int>()
-var dd = [1]
-dd.append(1)
-ff.dropFirst()
+var safeArray = ThreadSafeArray<Int>()
+let writeQueue = DispatchQueue(label: "write", attributes: .concurrent)
+let writeGroup = DispatchGroup()
 
-let sd = ff[0]
+for i in 0..<10 {
+    writeQueue.async(group: writeGroup) {
+        safeArray.append(i)
+    }
+}
 
+writeGroup.wait()
+
+let readQueue = DispatchQueue(label: "read", attributes: .concurrent)
+let readGroup = DispatchGroup()
+
+for i in 0..<safeArray.endIndex {
+    readQueue.async(group: readGroup) {
+        let element = safeArray[i]
+        print("element at index \(i): \(element)")
+    }
+}
+
+readGroup.wait()
